579d4281bb7e3c3fcd75a08137705c7df31a5291
wenqian157 <yang@arch.ethz.ch>
2023-01-31 10:29:03 +0100

    Update HDMeshSubdivision.cs

--- Runtime/HDMeshSubdivision.cs
+++ Runtime/HDMeshSubdivision.cs
@@ -69,7 +69,100 @@ namespace HD
 
             return new_faces_vertices;
         }
+         public static List<Vector3[]> subdivide_face_grid(Vector3[] face_vertices, int nU, int nV)
+        {
+            //        """
+            //splits a triangle, quad or a rectangle into a regular grid
+            //"""
+            //if len(face.vertices) > 4:
+            //    print('too many vertices')
+            //    return face
 
+            List<Vector3[]> new_faces_vertices = new List<Vector3[]>();
+            if (face_vertices.Length == 4)
+            {
+                List<Vector3> vsU1 = _vertices_betweem(face_vertices[0], face_vertices[1], nU);
+                List<Vector3> vsU2 = _vertices_betweem(face_vertices[3], face_vertices[2], nU);
+
+                List<List<Vector3>> gridVertices = new List<List<Vector3>>();
+                for (int i = 0; i < vsU1.Count; i++)
+                {
+                    gridVertices.Add(_vertices_betweem(vsU1[i], vsU2[i], nV));
+                }
+
+                List<Vector3[]> faces = new List<Vector3[]>();
+                for (int u = 0; u < vsU1.Count -1; u++)
+                {
+                    List<Vector3> vs1 = gridVertices[u];
+                    List<Vector3> vs2 = gridVertices[u+1];
+                    for (int v = 0; v < vs1.Count - 1; v++)
+                    {
+                        Vector3[] face = new Vector3[] {vs1[v], vs1[v+1], vs2[v+1], vs2[v]};
+                        // face_copy_properties(face, f) ? color and group 
+                        faces.Add(face);
+                    }
+                }
+                return faces;
+            }
+
+            //if len(face.vertices) == 4:
+            //    vsU1 = _vertices_between(face.vertices[0], face.vertices[1], nU)
+            //    vsU2 = _vertices_between(face.vertices[3], face.vertices[2], nU)
+            //    gridVertices = []
+            //    for u in range(len(vsU1)):
+            //        gridVertices.append(_vertices_between(vsU1[u], vsU2[u], nV))
+            //    faces = []
+            //    for u in range(len(vsU1) - 1):
+            //        vs1 = gridVertices[u]
+            //        vs2 = gridVertices[u + 1]
+            //        for v in range(len(vs1) - 1):
+            //            #f = Face([vs1[v], vs1[v + 1], vs2[v + 1], vs2[v]])
+            //            f = Face([vs1[v], vs2[v], vs2[v + 1], vs1[v + 1]])
+            //                    utils_face.face_copy_properties(face, f)
+            //            faces.append(f)
+            //    return faces
+
+            //if len(face.vertices) == 3:
+            //    vsU1 = _vertices_between(face.vertices[0], face.vertices[1], nU)
+            //    vsU2 = _vertices_between(face.vertices[0], face.vertices[2], nU)
+            //    gridVertices = []
+            //    for u in range(1, len(vsU1)):
+            //        gridVertices.append(_vertices_between(vsU1[u], vsU2[u], nV))
+            //    faces = []
+            //    # triangles
+            //    v0 = face.vertices[0]
+            //    vs1 = gridVertices[0]
+            //    for v in range(len(vs1) - 1):
+            //        f = Face([v0, vs1[v], vs1[v + 1]])
+            //                utils_face.face_copy_properties(face, f)
+            //        faces.append(f)
+            //    for u in range(len(gridVertices) - 1):
+            //        vs1 = gridVertices[u]
+            //        vs2 = gridVertices[u + 1]
+            //        for v in range(len(vs1) - 1):
+            //            f = Face([vs1[v], vs1[v + 1], vs2[v + 1], vs2[v]])
+            //                utils_face.face_copy_properties(face, f)
+            //            faces.append(f)
+            //    return faces
+
+            return new_faces_vertices;
+        }
+
+        public static List<Vector3> _vertices_betweem(Vector3 v1, Vector3 v2, int n)
+        {
+            //row = []
+            //deltaV = utils_vertex.vertex_subtract(v2, v1)
+            //deltaV = utils_vertex.vertex_divide(deltaV, n)
+            //for i in range(n):
+            //    addV = utils_vertex.vertex_scale(deltaV, i)
+            //    row.append(utils_vertex.vertex_add(addV, v1))
+            //row.append(v2)
+            //return row
+
+            List<Vector3> rowList = new List<Vector3>();
+            Vector3 deltaV = (v1 - v2)/n;
+
+<<<<<<< Updated upstream
         public static HDMesh subdivide_mesh_extrude(HDMesh hdMesh, float extrudeHeight)
         {
             HDMesh newMesh = new HDMesh();
@@ -84,6 +177,19 @@ namespace HD
             return newMesh;
         }
 
+=======
+            for(int i = 0; i < n; i++)
+            {
+                Vector3 addV = deltaV * i + v1;
+                rowList.Add(addV);
+            }
+            rowList.Add(v2);
+
+            //Vector3[] rowArray = rowList.ToArray();
+
+            return rowList;
+        }
+>>>>>>> Stashed changes
     }
 }
 
